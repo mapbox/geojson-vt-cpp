f9c6cdf34b3ba4163e495d604c17bf937211ac17
Dane Springmeyer <dane@mapbox.com>
2016-05-03 22:37:24 -0700

    optimize transformTile

--- src/transform.cpp
+++ src/transform.cpp
@@ -5,12 +5,18 @@
 namespace mapbox {
 namespace geojsonvt {
 
+inline int16_t trans_x(double x, uint16_t extent, uint32_t z2, uint32_t tx) {
+    return std::round(extent * (x * z2 - tx));
+}
+
+inline int16_t trans_y(double y, uint16_t extent, uint32_t z2, uint32_t ty) {
+    return std::round(extent * (y * z2 - ty));
+}
+
 TilePoint Transform::transformPoint(
     const ProjectedPoint& p, uint16_t extent, uint32_t z2, uint32_t tx, uint32_t ty) {
-
-    int16_t x = ::round(extent * (p.x * z2 - tx));
-    int16_t y = ::round(extent * (p.y * z2 - ty));
-
+    int16_t x = trans_x(p.x, extent, z2,tx);
+    int16_t y = trans_y(p.y, extent, z2,ty);
     return TilePoint(x, y);
 }
 
@@ -32,7 +38,7 @@ const Tile& Transform::transformTile(Tile& tile, uint16_t extent) {
             auto const& projected_points = geom.get<ProjectedPoints>();
             tileGeom.reserve(projected_points.size());
             for (const auto& pt : projected_points) {
-                tileGeom.push_back(transformPoint(pt, extent, z2, tx, ty));
+                tileGeom.emplace_back(trans_x(pt.x,extent,z2,tx),trans_x(pt.y,extent,z2,ty));
             }
 
         } else {
@@ -41,8 +47,8 @@ const Tile& Transform::transformTile(Tile& tile, uint16_t extent) {
             for (const auto& r : geom.get<ProjectedRings>()) {
                 TilePoints ring;
                 ring.reserve(r.points.size());
-                for (const auto& p : r.points) {
-                    ring.push_back(transformPoint(p, extent, z2, tx, ty));
+                for (const auto& pt : r.points) {
+                    ring.emplace_back(trans_x(pt.x,extent,z2,tx),trans_x(pt.y,extent,z2,ty));
                 }
                 tileGeom.push_back(std::move(ring));
             }
