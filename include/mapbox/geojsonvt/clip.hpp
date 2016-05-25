044cb2156f1e4a5c065c229bba81541cd570b7c2
Vladimir Agafonkin <agafonkin@gmail.com>
2016-05-25 19:36:01 +0300

    dump new geometry-based geojsonvt
    (lacking getTile and tests + viz updates)

--- include/mapbox/geojsonvt/clip.hpp
+++ include/mapbox/geojsonvt/clip.hpp
@@ -1,46 +1,226 @@
-#ifndef MAPBOX_GEOJSONVT_CLIP
-#define MAPBOX_GEOJSONVT_CLIP
+#pragma once
 
-#include "types.hpp"
-
-#include <vector>
+#include <mapbox/geojsonvt/types.hpp>
 
 namespace mapbox {
 namespace geojsonvt {
 
-class __attribute__((visibility("default"))) Clip {
-private:
-    // This class has only static functions; disallow creating instances of it.
-    Clip() = delete;
+/* clip features between two axis-parallel lines:
+ *     |        |
+ *  ___|___     |     /
+ * /   |   \____|____/
+ *     |        |
+ */
 
-public:
-    typedef ProjectedPoint (*IntersectCallback)(const ProjectedPoint&,
-                                                const ProjectedPoint&,
-                                                double);
+template <typename IntersectCallback>
+inline vt_features clip(const vt_features& features,
+                        const double k1,
+                        const double k2,
+                        const uint8_t axis,
+                        const IntersectCallback intersect,
+                        const double minAll,
+                        const double maxAll) {
 
-    static std::vector<ProjectedFeature> clip(const std::vector<ProjectedFeature>& features,
-                                              uint32_t scale,
-                                              double k1,
-                                              double k2,
-                                              uint8_t axis,
-                                              IntersectCallback intersect,
-                                              double minAll,
-                                              double maxAll);
+    if (minAll >= k1 && maxAll <= k2) // trivial accept
+        return features;
 
-private:
-    static ProjectedPoints
-    clipPoints(const ProjectedPoints& points, double k1, double k2, uint8_t axis);
-    static ProjectedRings clipGeometry(const ProjectedRings& rings,
-                                       double k1,
-                                       double k2,
-                                       uint8_t axis,
-                                       IntersectCallback intersect,
-                                       bool closed);
-    static ProjectedRing
-    newSlice(ProjectedRings& slices, ProjectedRing& slice, double area, double dist);
-};
+    if (minAll > k2 || maxAll < k1) // trivial reject
+        return {};
+
+    vt_features clipped;
+
+    for (const auto& feature : features) {
+        const auto& geom = feature.geometry;
+        const auto& props = feature.properties;
+
+        const double min = (axis == 0 ? feature.bbox.min.x : feature.bbox.min.y);
+        const double max = (axis == 0 ? feature.bbox.max.x : feature.bbox.max.y);
+
+        if (min >= k1 && max <= k2) { // trivial accept
+            clipped.push_back(feature);
+
+        } else if (min > k2 || max < k1) { // trivial reject
+            continue;
+
+        } else if (geom.is<vt_multi_point>()) {
+            const auto& points = geom.get<vt_multi_point>();
+            vt_multi_point part;
+            for (const auto& p : points) {
+                const double ak = (axis == 0 ? p.x : p.y);
+                if (ak >= k1 && ak <= k2)
+                    part.push_back(p);
+            }
+            clipped.emplace_back(part, props);
+
+        } else if (geom.is<vt_line_string>()) {
+            vt_multi_line_string parts;
+            clipLine(geom.get<vt_line_string>(), k1, k2, axis, intersect, parts);
+            if (parts.size() == 1)
+                clipped.emplace_back(parts[0], props);
+            else
+                clipped.emplace_back(parts, props);
+
+        } else if (geom.is<vt_multi_line_string>()) {
+            vt_multi_line_string parts;
+            for (const auto& line : geom.get<vt_multi_line_string>()) {
+                clipLine(line, k1, k2, axis, intersect, parts);
+            }
+            if (parts.size() == 1)
+                clipped.emplace_back(parts[0], props);
+            else
+                clipped.emplace_back(parts, props);
+
+        } else if (geom.is<vt_polygon>()) {
+            vt_polygon result;
+            for (const auto& ring : geom.get<vt_polygon>()) {
+                const auto new_ring = clipRing(ring, k1, k2, axis, intersect);
+                if (!new_ring.empty())
+                    result.push_back(new_ring);
+            }
+            clipped.emplace_back(result, props);
+
+        } else if (geom.is<vt_multi_polygon>()) {
+            vt_multi_polygon result;
+            for (const auto& polygon : geom.get<vt_multi_polygon>()) {
+                vt_polygon p;
+                for (const auto& ring : polygon) {
+                    const auto new_ring = clipRing(ring, k1, k2, axis, intersect);
+                    if (!new_ring.empty())
+                        p.push_back(new_ring);
+                }
+                if (!p.empty())
+                    result.push_back(p);
+            }
+            clipped.emplace_back(result, props);
+
+        } else {
+            throw std::runtime_error("Unexpected geometry type");
+        }
+    }
+
+    return clipped;
+}
+
+inline vt_line_string newSlice(vt_multi_line_string& parts, vt_line_string& slice, double dist) {
+    if (!slice.empty()) {
+        slice.dist = dist;
+        parts.push_back(std::move(slice));
+    }
+    return {};
+}
+
+template <typename IntersectCallback>
+inline void clipLine(const vt_line_string& line,
+                     double k1,
+                     double k2,
+                     uint8_t axis,
+                     IntersectCallback intersect,
+                     vt_multi_line_string& slices) {
+
+    const double dist = line.dist;
+    const size_t len = line.size();
+
+    vt_line_string slice;
+
+    for (size_t i = 0; i < (len - 1); ++i) {
+        const auto& a = line[i];
+        const auto& b = line[i + 1];
+        const double ak = axis == 0 ? a.x : a.y;
+        const double bk = axis == 0 ? b.x : b.y;
+
+        if (ak < k1) {
+            if (bk > k2) { // ---|-----|-->
+                slice.push_back(intersect(a, b, k1));
+                slice.push_back(intersect(a, b, k2));
+                slice = newSlice(slices, slice, dist);
+
+            } else if (bk >= k1) { // ---|-->  |
+                slice.push_back(intersect(a, b, k1));
+                if (i == len - 2)
+                    slice.push_back(b); // last point
+            }
+        } else if (ak > k2) {
+            if (bk < k1) { // <--|-----|---
+                slice.push_back(intersect(a, b, k2));
+                slice.push_back(intersect(a, b, k1));
+                slice = newSlice(slices, slice, dist);
+
+            } else if (bk <= k2) { // |  <--|---
+                slice.push_back(intersect(a, b, k2));
+                if (i == len - 2)
+                    slice.push_back(b); // last point
+            }
+        } else {
+            slice.push_back(a);
+
+            if (bk < k1) { // <--|---  |
+                slice.push_back(intersect(a, b, k1));
+                slice = newSlice(slices, slice, dist);
+
+            } else if (bk > k2) { // |  ---|-->
+                slice.push_back(intersect(a, b, k2));
+                slice = newSlice(slices, slice, dist);
+            }
+            // | --> |
+        }
+    }
+
+    // add the final slice
+    newSlice(slices, slice, dist);
+}
+
+template <typename IntersectCallback>
+inline vt_linear_ring clipRing(
+    const vt_linear_ring& ring, double k1, double k2, uint8_t axis, IntersectCallback intersect) {
+
+    const size_t len = ring.size();
+
+    vt_linear_ring slice;
+    slice.area = ring.area;
+
+    for (size_t i = 0; i < (len - 1); ++i) {
+        const auto& a = ring[i];
+        const auto& b = ring[i + 1];
+        const double ak = axis == 0 ? a.x : a.y;
+        const double bk = axis == 0 ? b.x : b.y;
+
+        if (ak < k1) {
+            if (bk >= k1) {
+                slice.push_back(intersect(a, b, k1)); // ---|-->  |
+                if (bk > k2)                          // ---|-----|-->
+                    slice.push_back(intersect(a, b, k2));
+                else if (i == len - 2)
+                    slice.push_back(b); // last point
+            }
+        } else if (ak > k2) {
+            if (bk <= k2) { // |  <--|---
+                slice.push_back(intersect(a, b, k2));
+                if (bk < k1) // <--|-----|---
+                    slice.push_back(intersect(a, b, k1));
+                else if (i == len - 2)
+                    slice.push_back(b); // last point
+            }
+        } else {
+            slice.push_back(a);
+            if (bk < k1) // <--|---  |
+                slice.push_back(intersect(a, b, k1));
+            else if (bk > k2) // |  ---|-->
+                slice.push_back(intersect(a, b, k2));
+            // | --> |
+        }
+    }
+
+    // close the polygon if its endpoints are not the same after clipping
+    if (!slice.empty()) {
+        const auto& first = slice.front();
+        const auto& last = slice.back();
+        if (first != last) {
+            slice.push_back(first);
+        }
+    }
+
+    return slice;
+}
 
 } // namespace geojsonvt
 } // namespace mapbox
-
-#endif // MAPBOX_GEOJSONVT_CLIP
--- include/mapbox/geojsonvt/tile.hpp
+++ include/mapbox/geojsonvt/tile.hpp
@@ -1,43 +1,187 @@
-#ifndef MAPBOX_GEOJSONVT_TILE
-#define MAPBOX_GEOJSONVT_TILE
+#pragma once
 
-#include "types.hpp"
+#include <mapbox/geojsonvt/types.hpp>
 
 namespace mapbox {
 namespace geojsonvt {
 
-class __attribute__((visibility("default"))) Tile {
+class Tile {
 public:
-    static Tile createTile(std::vector<ProjectedFeature> const& features,
-                           uint32_t z2,
-                           uint32_t tx,
-                           uint32_t ty,
-                           double tolerance,
-                           bool noSimplify);
+    vt_features source_features;
+    tile_features features;
 
-    static void
-    addFeature(Tile& tile, const ProjectedFeature& feature, double tolerance, bool noSimplify);
+    const uint8_t z;
+    const uint32_t x;
+    const uint32_t y;
+    const uint16_t extent;
+    const double tolerance;
+    bool is_solid = false;
 
-    inline operator bool() const {
-        return this->numPoints != 0;
+    uint32_t num_points = 0;
+    uint32_t num_simplified = 0;
+    mapbox::geometry::box<double> bbox = { { 2, 1 }, { -1, 0 } };
+
+    Tile(const vt_features& source,
+         const uint8_t z_,
+         const uint32_t x_,
+         const uint32_t y_,
+         const uint16_t extent_,
+         const uint16_t buffer,
+         const double tolerance_)
+        : z(z_),
+          x(x_),
+          y(y_),
+          extent(extent_),
+          tolerance(tolerance_),
+          z2(std::pow(2, z_)),
+          sq_tolerance(tolerance_ * tolerance_) {
+
+        for (const auto& feature : source) {
+            const auto& geom = feature.geometry;
+            const auto& props = feature.properties;
+
+            num_points += feature.num_points;
+
+            if (geom.is<vt_point>()) {
+                features.push_back({ { transform(geom.get<vt_point>()) }, props });
+
+            } else if (geom.is<vt_multi_point>()) {
+                features.push_back({ { transform(geom.get<vt_multi_point>()) }, props });
+
+            } else if (geom.is<vt_line_string>()) {
+                const auto line = transform(geom.get<vt_line_string>());
+                if (!line.empty())
+                    features.push_back({ { line }, props });
+
+            } else if (geom.is<vt_multi_line_string>()) {
+                const auto lines = transform(geom.get<vt_multi_line_string>());
+                const auto size = lines.size();
+                if (size == 1)
+                    features.push_back({ { lines[0] }, props });
+                else if (size > 1)
+                    features.push_back({ { lines }, props });
+
+            } else if (geom.is<vt_polygon>()) {
+                const auto polygon = transform(geom.get<vt_polygon>());
+                if (!polygon.empty())
+                    features.push_back({ { polygon }, props });
+
+            } else if (geom.is<vt_multi_polygon>()) {
+                const auto polygons = transform(geom.get<vt_multi_polygon>());
+                const auto size = polygons.size();
+                if (size == 1)
+                    features.push_back({ { polygons[0] }, props });
+                else if (size > 1)
+                    features.push_back({ { polygons }, props });
+
+            } else {
+                throw std::runtime_error("Geometry type not supported");
+            }
+
+            bbox.min.x = std::min(feature.bbox.min.x, bbox.min.x);
+            bbox.min.y = std::min(feature.bbox.min.y, bbox.min.y);
+            bbox.max.x = std::max(feature.bbox.max.x, bbox.max.x);
+            bbox.max.y = std::max(feature.bbox.max.y, bbox.max.y);
+        }
+
+        is_solid = isSolid(buffer);
     }
 
-public:
-    std::vector<TileFeature> features;
-    uint32_t numPoints = 0;
-    uint32_t numSimplified = 0;
-    uint32_t numFeatures = 0;
-    uint32_t z2 = 0;
-    uint32_t tx = 0;
-    uint32_t ty = 0;
-    bool transformed = false;
-    ProjectedPoint min{ 2, 1 };
-    ProjectedPoint max{ -1, 0 };
+private:
+    const double z2;
+    const double sq_tolerance;
 
-    std::vector<ProjectedFeature> source;
+    bool isSolid(const uint16_t buffer) {
+        if (features.size() != 1)
+            return false;
+
+        const auto& geom = features.front().geometry;
+        if (!geom.is<tile_polygon>())
+            return false;
+
+        const auto& rings = geom.get<tile_polygon>();
+        if (rings.size() > 1)
+            return false;
+
+        const auto& ring = rings.front();
+        if (ring.size() != 5)
+            return false;
+
+        const int16_t min = -static_cast<int16_t>(buffer);
+        const int16_t max = static_cast<int16_t>(extent + buffer);
+        for (const auto& p : ring) {
+            if ((p.x != min && p.x != max) || (p.y != min && p.y != max))
+                return false;
+        }
+
+        return true;
+    }
+
+    tile_point transform(const vt_point& p) {
+        ++num_simplified;
+        return { static_cast<int16_t>(std::round((p.x * z2 - x) * extent)),
+                 static_cast<int16_t>(std::round((p.y * z2 - y) * extent)) };
+    }
+
+    tile_multi_point transform(const vt_multi_point& points) {
+        tile_multi_point result;
+        result.reserve(points.size());
+        for (const auto& p : points) {
+            result.push_back(transform(p));
+        }
+        return result;
+    }
+
+    tile_line_string transform(const vt_line_string& line) {
+        tile_line_string result;
+        if (line.dist > tolerance) {
+            for (const auto& p : line) {
+                if (p.z > sq_tolerance)
+                    result.push_back(transform(p));
+            }
+        }
+        return result;
+    }
+
+    tile_linear_ring transform(const vt_linear_ring& ring) {
+        tile_linear_ring result;
+        if (ring.area > sq_tolerance) {
+            for (const auto& p : ring) {
+                if (p.z > sq_tolerance)
+                    result.push_back(transform(p));
+            }
+        }
+        return result;
+    }
+
+    tile_multi_line_string transform(const vt_multi_line_string& lines) {
+        tile_multi_line_string result;
+        for (const auto& line : lines) {
+            if (line.dist > tolerance)
+                result.push_back(transform(line));
+        }
+        return result;
+    }
+
+    tile_polygon transform(const vt_polygon& rings) {
+        tile_polygon result;
+        for (const auto& ring : rings) {
+            if (ring.area > sq_tolerance)
+                result.push_back(transform(ring));
+        }
+        return result;
+    }
+
+    tile_multi_polygon transform(const vt_multi_polygon& polygons) {
+        tile_multi_polygon result;
+        for (const auto& polygon : polygons) {
+            const auto p = transform(polygon);
+            if (!p.empty())
+                result.push_back(p);
+        }
+        return result;
+    }
 };
 
 } // namespace geojsonvt
 } // namespace mapbox
-
-#endif // MAPBOX_GEOJSONVT_TILE
