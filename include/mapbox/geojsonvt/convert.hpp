b79a26fb3a8997330b792b3971355091c3dca07f
Vladimir Agafonkin <agafonkin@gmail.com>
2016-05-25 23:40:01 +0300

    avoid using namespace in header

--- include/mapbox/geojsonvt/convert.hpp
+++ include/mapbox/geojsonvt/convert.hpp
@@ -6,12 +6,11 @@
 
 #include <cmath>
 
-using namespace mapbox::geometry;
-
 namespace mapbox {
 namespace geojsonvt {
 
-inline vt_point project(const point<double>& p, __attribute__((unused)) const double tolerance) {
+inline vt_point project(const mapbox::geometry::point<double>& p,
+                        __attribute__((unused)) const double tolerance) {
     const double sine = std::sin(p.y * M_PI / 180);
     const double x = p.x / 360 + 0.5;
     const double y =
@@ -19,7 +18,8 @@ inline vt_point project(const point<double>& p, __attribute__((unused)) const do
     return { x, y, 0.0 };
 }
 
-inline vt_line_string project(const line_string<double>& points, const double tolerance) {
+inline vt_line_string project(const mapbox::geometry::line_string<double>& points,
+                              const double tolerance) {
     vt_line_string result;
     const size_t len = points.size();
     result.reserve(len);
@@ -40,7 +40,8 @@ inline vt_line_string project(const line_string<double>& points, const double to
     return result;
 }
 
-inline vt_linear_ring project(const linear_ring<double>& ring, const double tolerance) {
+inline vt_linear_ring project(const mapbox::geometry::linear_ring<double>& ring,
+                              const double tolerance) {
     vt_linear_ring result;
     const size_t len = ring.size();
     result.reserve(len);
@@ -63,7 +64,8 @@ inline vt_linear_ring project(const linear_ring<double>& ring, const double tole
     return result;
 }
 
-inline vt_multi_point project(const multi_point<double>& points, const double tolerance) {
+inline vt_multi_point project(const mapbox::geometry::multi_point<double>& points,
+                              const double tolerance) {
     vt_multi_point result;
     result.reserve(points.size());
     for (const auto& p : points) {
@@ -72,7 +74,7 @@ inline vt_multi_point project(const multi_point<double>& points, const double to
     return result;
 }
 
-inline vt_multi_line_string project(const multi_line_string<double>& lines,
+inline vt_multi_line_string project(const mapbox::geometry::multi_line_string<double>& lines,
                                     const double tolerance) {
     vt_multi_line_string result;
     result.reserve(lines.size());
@@ -83,7 +85,7 @@ inline vt_multi_line_string project(const multi_line_string<double>& lines,
     return result;
 }
 
-inline vt_polygon project(const polygon<double>& rings, const double tolerance) {
+inline vt_polygon project(const mapbox::geometry::polygon<double>& rings, const double tolerance) {
     vt_polygon result;
     result.reserve(rings.size());
 
@@ -93,7 +95,8 @@ inline vt_polygon project(const polygon<double>& rings, const double tolerance) 
     return result;
 }
 
-inline vt_multi_polygon project(const multi_polygon<double>& polygons, const double tolerance) {
+inline vt_multi_polygon project(const mapbox::geometry::multi_polygon<double>& polygons,
+                                const double tolerance) {
     vt_multi_polygon result;
     result.reserve(polygons.size());
 
@@ -103,8 +106,9 @@ inline vt_multi_polygon project(const multi_polygon<double>& polygons, const dou
     return result;
 }
 
-inline vt_geometry project(const ::geometry<double>& geom, const double tolerance) {
-    return ::geometry<double>::visit(geom, [&] (const auto& g) { return vt_geometry(project(g, tolerance)); });
+inline vt_geometry project(const mapbox::geometry::geometry<double>& geom, const double tolerance) {
+    return mapbox::geometry::geometry<double>::visit(
+        geom, [&](const auto& g) { return vt_geometry(project(g, tolerance)); });
 }
 
 inline vt_features convert(const geojson_features& features, const double tolerance) {
