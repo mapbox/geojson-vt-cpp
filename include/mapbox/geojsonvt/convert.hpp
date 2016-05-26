6f9a1af7172f4fa2251e6a486ff73f757db498c6
Vladimir Agafonkin <agafonkin@gmail.com>
2016-05-26 13:16:14 +0300

    attempt at visitor pattern in project code

--- include/mapbox/geojsonvt/convert.hpp
+++ include/mapbox/geojsonvt/convert.hpp
@@ -10,107 +10,104 @@ namespace mapbox {
 namespace geojsonvt {
 namespace detail {
 
-inline vt_point project(const mapbox::geometry::point<double>& p,
-                        __attribute__((unused)) const double tolerance) {
-    const double sine = std::sin(p.y * M_PI / 180);
-    const double x = p.x / 360 + 0.5;
-    const double y =
-        std::max(std::min(0.5 - 0.25 * std::log((1 + sine) / (1 - sine)) / M_PI, 1.0), 0.0);
-    return { x, y, 0.0 };
-}
+struct Converter {
+    const double tolerance;
 
-inline vt_line_string project(const mapbox::geometry::line_string<double>& points,
-                              const double tolerance) {
-    vt_line_string result;
-    const size_t len = points.size();
-    result.reserve(len);
-
-    for (const auto& p : points) {
-        result.push_back(project(p, tolerance));
+    Converter(const double tolerance_) : tolerance(tolerance_) {
     }
 
-    for (size_t i = 0; i < len - 1; ++i) {
-        const auto& a = result[i];
-        const auto& b = result[i + 1];
-        // use Manhattan distance instead of Euclidian to avoid expensive square root computation
-        result.dist += std::abs(b.x - a.x) + std::abs(b.y - a.y);
+    vt_point operator()(const mapbox::geometry::point<double>& p) {
+        const double sine = std::sin(p.y * M_PI / 180);
+        const double x = p.x / 360 + 0.5;
+        const double y =
+            std::max(std::min(0.5 - 0.25 * std::log((1 + sine) / (1 - sine)) / M_PI, 1.0), 0.0);
+        return { x, y, 0.0 };
     }
 
-    simplify(result, tolerance);
+    vt_line_string operator()(const mapbox::geometry::line_string<double>& points) {
+        vt_line_string result;
+        const size_t len = points.size();
+        result.reserve(len);
 
-    return result;
-}
+        for (const auto& p : points) {
+            result.push_back(operator()(p));
+        }
 
-inline vt_linear_ring project(const mapbox::geometry::linear_ring<double>& ring,
-                              const double tolerance) {
-    vt_linear_ring result;
-    const size_t len = ring.size();
-    result.reserve(len);
+        for (size_t i = 0; i < len - 1; ++i) {
+            const auto& a = result[i];
+            const auto& b = result[i + 1];
+            // use Manhattan distance instead of Euclidian to avoid expensive square root
+            // computation
+            result.dist += std::abs(b.x - a.x) + std::abs(b.y - a.y);
+        }
 
-    for (const auto& p : ring) {
-        result.push_back(project(p, tolerance));
+        simplify(result, tolerance);
+
+        return result;
     }
 
-    double area = 0.0;
+    vt_linear_ring operator()(const mapbox::geometry::linear_ring<double>& ring) {
+        vt_linear_ring result;
+        const size_t len = ring.size();
+        result.reserve(len);
 
-    for (size_t i = 0; i < len - 1; ++i) {
-        const auto& a = result[i];
-        const auto& b = result[i + 1];
-        area += a.x * b.y - b.x * a.y;
+        for (const auto& p : ring) {
+            result.push_back(operator()(p));
+        }
+
+        double area = 0.0;
+
+        for (size_t i = 0; i < len - 1; ++i) {
+            const auto& a = result[i];
+            const auto& b = result[i + 1];
+            area += a.x * b.y - b.x * a.y;
+        }
+        result.area = std::abs(area / 2);
+
+        simplify(result, tolerance);
+
+        return result;
     }
-    result.area = std::abs(area / 2);
 
-    simplify(result, tolerance);
-
-    return result;
-}
-
-inline vt_multi_point project(const mapbox::geometry::multi_point<double>& points,
-                              const double tolerance) {
-    vt_multi_point result;
-    result.reserve(points.size());
-    for (const auto& p : points) {
-        result.push_back(project(p, tolerance));
+    vt_multi_point operator()(const mapbox::geometry::multi_point<double>& points) {
+        vt_multi_point result;
+        result.reserve(points.size());
+        for (const auto& p : points) {
+            result.push_back(operator()(p));
+        }
+        return result;
     }
-    return result;
-}
 
-inline vt_multi_line_string project(const mapbox::geometry::multi_line_string<double>& lines,
-                                    const double tolerance) {
-    vt_multi_line_string result;
-    result.reserve(lines.size());
+    vt_multi_line_string operator()(const mapbox::geometry::multi_line_string<double>& lines) {
+        vt_multi_line_string result;
+        result.reserve(lines.size());
 
-    for (const auto& line : lines) {
-        result.push_back(project(line, tolerance));
+        for (const auto& line : lines) {
+            result.push_back(operator()(line));
+        }
+        return result;
     }
-    return result;
-}
 
-inline vt_polygon project(const mapbox::geometry::polygon<double>& rings, const double tolerance) {
-    vt_polygon result;
-    result.reserve(rings.size());
+    vt_polygon operator()(const mapbox::geometry::polygon<double>& rings) {
+        vt_polygon result;
+        result.reserve(rings.size());
 
-    for (const auto& ring : rings) {
-        result.push_back(project(ring, tolerance));
+        for (const auto& ring : rings) {
+            result.push_back(operator()(ring));
+        }
+        return result;
     }
-    return result;
-}
 
-inline vt_multi_polygon project(const mapbox::geometry::multi_polygon<double>& polygons,
-                                const double tolerance) {
-    vt_multi_polygon result;
-    result.reserve(polygons.size());
+    vt_multi_polygon operator()(const mapbox::geometry::multi_polygon<double>& polygons) {
+        vt_multi_polygon result;
+        result.reserve(polygons.size());
 
-    for (const auto& polygon : polygons) {
-        result.push_back(project(polygon, tolerance));
+        for (const auto& polygon : polygons) {
+            result.push_back(operator()(polygon));
+        }
+        return result;
     }
-    return result;
-}
-
-inline vt_geometry project(const mapbox::geometry::geometry<double>& geom, const double tolerance) {
-    return mapbox::geometry::geometry<double>::visit(
-        geom, [&](const auto& g) { return vt_geometry(project(g, tolerance)); });
-}
+};
 
 inline vt_features convert(const mapbox::geometry::feature_collection<double>& features,
                            const double tolerance) {
@@ -118,7 +115,9 @@ inline vt_features convert(const mapbox::geometry::feature_collection<double>& f
     projected.reserve(features.size());
 
     for (const auto& feature : features) {
-        projected.emplace_back(project(feature.geometry, tolerance), feature.properties);
+        projected.emplace_back(vt_geometry(mapbox::geometry::geometry<double>::visit(
+                                   feature.geometry, Converter(tolerance))),
+                               feature.properties);
     }
     return projected;
 }
